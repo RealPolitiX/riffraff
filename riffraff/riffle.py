eb74938ce1cbc0d0c8daf00e3e25a87e20265aeb
RealPolitiX <xrpatrick@gmail.com>
2020-11-22 06:45:55 -0600

    Updated riffle function

--- riffraff/riffle.py
+++ riffraff/riffle.py
@@ -17,12 +17,12 @@ def riffle(*arr):
         An array with interleaving elements from each input array.
     """
 
-    arr = (map(np.ravel, arr))
-    arrlen = np.array(map(len, arr))
+    arrtmp = list(map(np.ravel, arr))
+    arrlen = list(map(len, arrtmp))
     minlen = np.min(arrlen)
     if not(np.prod(arrlen == minlen)):
-        arr = [a[:minlen] for a in arr]
+        arrtmp = [a[:minlen] for a in arrtmp]
     
-    riffarr = np.vstack(arr).reshape((-1,), order='F')
+    riffarr = np.vstack(arrtmp).reshape((-1,), order='F')
 
     return riffarr
